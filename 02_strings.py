1ec16e2f2c969dcd261fb8eb7f45c83081f635fa
iabhishekpratap <iapsingh@outlook.com>
2024-11-15 13:01:07 +0530

    commit new file

---
+++ 02_strings.py
@@ -0,0 +1,15 @@
+name1="paul" #assign string in variable
+name2="filip"
+
+#add 2 strings
+print(name1+' '+name2) #add space with quote""
+
+#change case of string
+full=(name1+" "+name2)
+print(full.upper())
+print(full.lower())
+
+msg="you are, my frind"
+print(msg.split())
+print(msg.split(",")) #comma "," used for the split given string into 2 parts
+
---
+++ 03_lists.py
@@ -0,0 +1,42 @@
+# names = "ram abhijeet aditya pankaj"
+# # print (names.split()) #give result in list
+
+# #define list
+list = ['ram', 'abhijeet', 'aditya', 'pankaj']
+
+# print(list[0])
+# print(list[2])
+
+# #add new user in list
+# list.append('super')
+# print(list)
+
+# x=len((list))
+# print(x)
+
+# used to store removed value in variable
+# x=list.pop()
+# print(x)
+
+# #slicing of list
+# print(list[0:2])
+# print(list[-2:])
+
+price=[2,3,4,5,6,7,8,9,9999,11,22,33,55,77]
+print(sum(price))
+print(min(price))
+print(max(price))
+
+
+mix_list=[1,"apple",3,"ball"]
+print(mix_list)
+
+
+
+
+
+
+
+
+
+
