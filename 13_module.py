c0cffff74e12c444c7c768070c3334cce0f90357
iabhishekpratap <iapsingh@outlook.com>
2024-11-17 12:08:37 +0530

    committed

--- 13_module.py
+++ 13_module.py
@@ -1,9 +1,20 @@
-import calc
+# import calc
 
-result=calc.add(10,20)
-print(result)
+# result=calc.add(10,20)
+# print(result)
 
-x=calc.evenodd(3235352)
-print(x)
+# x=calc.evenodd(3235352)
+# print(x)
 
 
+import emp
+
+#object emp1, emp2
+emp1 = emp.Employee("raju","raju@gmail.com","sales", 4000)
+emp2 = emp.Employee("shaym","shyam@gmail.com","hr", 40000)
+
+print(emp2.email)
+emp1.empinfo()
+emp2.empinfo()
+
+emp1.deptchange("devops")
--- emp.py
+++ emp.py
@@ -1,6 +1,66 @@
 class Employee:
-    pass
+    #constructor
+    '''constructor is a special method used to
+    initialize the attributes of a class 
+    when an object is created.'''
 
-emp1 = Employee()
-print(emp1)
+    '''The constructor ensures that certain actions 
+    (like initializing data or setting default values) 
+    happen automatically whenever a new object is created.'''
+
+#     def __init__(self): #this functional automatically called
+#         print("const called")
+
+# #object emp1
+# emp1 = Employee()
+#########################################################
+#     def __init__(self,name,email,dept,salary): #this functional automatically called
+#         self.name=name
+#         self.email=email
+#         self.dept=dept 
+#         self.salary=salary
+#         #self.email reperesent self(खुद का) mail of employe 
+#         #=email represent that we provide
+
+# #object emp1
+# emp1 = Employee("raju","raju@gmail.com","sales", 4000)
+# emp2 = Employee("shaym","shyam@gmail.com","hr", 40000)
+
+# print(emp2.email)
+##############################################################
+    def __init__(self,name,email,dept,salary): #this functional automatically called
+        self.name=name
+        self.email=email
+        self.dept=dept 
+        self.salary=salary
+
+    def empinfo(self):
+        print("name is ",self.name)
+        print("email is ",self.email)
+        print("department is ",self.dept)
+        print("salary is ",self.salary)
+
+    def deptchange(self,newdept):
+        self.dept=newdept
+        print("department change to newdept",newdept)
+
+
+# #object emp1, emp2
+# emp1 = Employee("raju","raju@gmail.com","sales", 4000)
+# emp2 = Employee("shaym","shyam@gmail.com","hr", 40000)
+
+# print(emp2.email)
+# emp1.empinfo()
+# emp2.empinfo()
+
+# emp1.deptchange("devops")
+
+######################################3
+
+# class Employee:
+#     pass
+
+# #object emp1
+# emp1 = Employee()
+# print(emp1)
 
